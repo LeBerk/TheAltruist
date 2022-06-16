035dc40db65fe2da01c324974a8578fdeff6bc5d
Berk Lee <87292724+LeBerk@users.noreply.github.com>
2022-06-17 00:50:45 +0900

    update heading padding

--- App.js
+++ App.js
@@ -66,9 +66,13 @@ const Category = () => {
         colors={['rgba(77, 0, 241, 0.62)', 'transparent']}
         style={category_styles.background}
       />
+      <View>
       <Text style={category_styles.header}>Categories!</Text>
       <Categories title='Help People' image="https://i.ibb.co/K7CtT6F/921356.png"/>
       <Categories title='Help the Planet' image="https://i.ibb.co/4gFjyhc/2072130.png"/>
+      </View>
+      <View style={category_styles.space}>
+      </View>
     </View>
   )
 }
@@ -77,9 +81,7 @@ const Categories = (props) => (
   <Pressable>
   <View style={category_styles.box}>
     <Image source={{ uri: props.image }} style={category_styles.image}/>
-    <View>
       <Text style={category_styles.category_title}>{props.title}</Text>
-    </View>
   </View>
   </Pressable>
 );
@@ -119,11 +121,15 @@ const category_styles = StyleSheet.create({
     color: '#FAFAFF',
     fontSize: 50, 
     fontWeight: '400', 
-    paddingTop: -200, 
+    paddingTop: -300, 
     alignItems: 'flex-start',
     justifyContent: 'flex-start', 
-    paddingBottom: 60
+    paddingBottom: 60,
+    paddingLeft: 10
   }, 
+  space: {
+    paddingBottom: 100
+  },
   background: {
     position: 'absolute',
     left: 0,
